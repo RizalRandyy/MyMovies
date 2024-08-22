2d86ea3023f7112ba7b6fc679bcbf65827b4a4eb
RizalRandyy <147943975+RizalRandyy@users.noreply.github.com>
2024-08-23 00:49:47 +0700

    Update page.jsx

--- src/app/search/[keyword]/page.jsx
+++ src/app/search/[keyword]/page.jsx
@@ -1,6 +1,7 @@
 import MoviePage from "@/components/MoviePage";
 import { getMovieResponse } from "@/libs/api-libs";
 import { authUserSession } from "@/libs/auth-libs";
+import prisma from "@/libs/prisma";
 
 const Page = async ({ params, searchParams }) => {
   const { keyword } = params;
